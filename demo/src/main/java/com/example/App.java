9c8e99f0c863d487884601c43f349f70d836447d
Michael D Lambert <mlambert@dolbey.com>
2024-04-23 00:16:50 +0000

    Initial commit

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println( "Hello World!" );
+    }
+}
